ceaf769cda74c1c9e2d0475c01cb23d2194dddd6
zacisco <zacisco@users.noreply.github.com>
2025-07-22 14:23:11 +0300

    Update progress_bar.sh
    add custom label

--- progress_bar.sh
+++ progress_bar.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+label="Progress: "
+
 # Percent Progress Bar Options
 bar_size=40
 bar_char_done="#"
@@ -10,12 +12,15 @@ show_percent_progress() {
   current="$1"
   total="$2"
   add_text="$3"
+  [ ! -z "$4" ] && label="$4"
+
   percent=$(( 100 * current / total ))
   done=$(( bar_size * percent / 100 ))
   left=$(( bar_size - done ))
   done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
   left_sub_bar=$(printf "%${left}s" | tr " " "${bar_char_left}")
-  echo -ne "\rProgress: [${done_sub_bar}${left_sub_bar}] ${percent}% ${add_text}"
+
+  echo -ne "\r${label}[${done_sub_bar}${left_sub_bar}] ${percent}% ${add_text}"
 }
 
 # Spinner Progress Bar Options
@@ -23,7 +28,8 @@ spinner_chars="/-\|"
 
 show_spinner_progress() {
   local progress=$(( ($1+1) %4 ))
-  local add_text="$2"
+  [ ! -z "$2" ] && label="$2"
   [ -z "${add_text}" ] && add_text="Progress:"
-  echo -ne "\r${add_text} ${spinner_chars:$progress:1}"
+
+  echo -ne "\r${label}${spinner_chars:$progress:1}"
 }
