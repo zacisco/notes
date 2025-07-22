2e264ffc3aa00581e76069453bba99c429044d5a
zacisco <zacisco@users.noreply.github.com>
2025-07-22 14:24:33 +0300

    Update progress_bar.sh

--- progress_bar.sh
+++ progress_bar.sh
@@ -9,16 +9,16 @@ bar_char_left=" "
 bar_percentage_scale=0
 
 show_percent_progress() {
-  current="$1"
-  total="$2"
-  add_text="$3"
+  local current="$1"
+  local total="$2"
+  local add_text="$3"
   [ ! -z "$4" ] && label="$4"
 
-  percent=$(( 100 * current / total ))
-  done=$(( bar_size * percent / 100 ))
-  left=$(( bar_size - done ))
-  done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
-  left_sub_bar=$(printf "%${left}s" | tr " " "${bar_char_left}")
+  local percent=$(( 100 * current / total ))
+  local done=$(( bar_size * percent / 100 ))
+  local left=$(( bar_size - done ))
+  local done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
+  local left_sub_bar=$(printf "%${left}s" | tr " " "${bar_char_left}")
 
   echo -ne "\r${label}[${done_sub_bar}${left_sub_bar}] ${percent}% ${add_text}"
 }
@@ -29,7 +29,6 @@ spinner_chars="/-\|"
 show_spinner_progress() {
   local progress=$(( ($1+1) %4 ))
   [ ! -z "$2" ] && label="$2"
-  [ -z "${add_text}" ] && add_text="Progress:"
 
   echo -ne "\r${label}${spinner_chars:$progress:1}"
 }
