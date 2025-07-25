7b66de030fb2b83e78ecc6259f0efca1f796732a
zacisco <zacisco@users.noreply.github.com>
2025-07-25 16:28:46 +0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -25,38 +25,39 @@ is_num() {
 
 # get version from build.gradle or gradle.properties (can add prefixes)
 gen_app_version() {
-  ONLY_READ=$1
-  BUILD_TYPE=$2
-  VER_SUFFIX=$3
+  local read_only=$1
+  local build_type=$2
+  local ver_suffix=$3
   # Prepare archive version suffix
   local app_ver=$(grep -E "^version" build.gradle | sed -r "s|^version .*?'(.*?)'|\1|")
   [ -z "${app_ver}" ] && app_ver=$(grep -E "^version" gradle.properties | sed -r "s/^version=//")
-  if [ -z "${ONLY_READ}" ]; then
-    [ "${BUILD_TYPE}" = "test" ] && app_ver+="_TEST"
-    [ ! -z "${VER_SUFFIX}" ] && app_ver+="_${VER_SUFFIX}"
+  if [ -z "${read_only}" ]; then
+    [ "${build_type}" = "test" ] && app_ver+="_TEST"
+    [ ! -z "${ver_suffix}" ] && app_ver+="_${ver_suffix}"
   fi
   echo $app_ver
-}
+} 
 
 check_and_get() {
-  local simple=$1
-  [ ! -z "${simple}" ] && GIT_ARGS="--depth=1"
-  if [ ! -f "${CMD_FILE}" ]; then
-    git -c advice.detachedHead=false clone $GIT_ARGS --branch=$BRANCH_or_TAG $gitlab
-    cd document
-    chmod +x $CMD_FILE
-    echo
+  local branch_or_tag="${1}"
+  local clone_url="${2}"
+  local cmd_file="${3}"
+  local simple="${4}"
+  [ ! -z "${simple}" ] && local git_args="--depth=1"
+  if [ ! -f "${cmd_file}" ]; then
+    git -c advice.detachedHead=false clone $git_args --branch=$branch_or_tag $clone_url
+    cd $(get_single_folder .) && chmod +x $cmd_file && echo
+  else
+    chmod +x $cmd_file
   fi
 }
 
 check_curl_status() {
   local val=$1
-  local stage=$2
-  local file=$3
+  local stage="${2}"
+  local file="${3}"
   is_uint $val
-  if [ $? -eq 0 ] && [ $val -ne 200 ]; then
-    echo "${stage} ERROR. Look at ${file}" && exit
-  fi
+  [ $? -eq 0 ] && [ $val -ne 200 ] && echo "${stage} ERROR. Look at ${file}" && exit
 }
 
 make_dir() {
