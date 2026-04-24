04b55d11974ac9ba687ec1510d9b6a009995e7e4
zacisco <zacisco@users.noreply.github.com>
2026-04-24 15:46:10 +0300

    Update WebPerplexity.js
    add remove overlay after repeat prompt

--- useful/WebPerplexity.js
+++ useful/WebPerplexity.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name          Web Perplexity Wide
-// @description	  Wide style for Perplexity Web application.
+// @name          Web Perplexity Fixes
+// @description	  Some fixes for Perplexity Web application.
 // @version       2026-04-24
 // @author        Z@C
 // @icon          https://www.perplexity.ai/favicon.svg
@@ -13,6 +13,7 @@
 (function() {
     'use strict';
 
+// ===== Make WIDE =====
     var css = [
         "/* FOR WIDEE */",
         "main div.isolate div.isolate div.max-w-threadContentWidth {",
@@ -23,21 +24,48 @@
     ].join("\n");
 
     if (typeof GM_addStyle != "undefined") {
-	GM_addStyle(css);
-} else if (typeof PRO_addStyle != "undefined") {
-	PRO_addStyle(css);
-} else if (typeof addStyle != "undefined") {
-	addStyle(css);
-} else {
-	var node = document.createElement("style");
-	node.type = "text/css";
-	node.appendChild(document.createTextNode(css));
-	var heads = document.getElementsByTagName("head");
-	if (heads.length > 0) {
-		heads[0].appendChild(node);
-	} else {
-		// no head yet, stick it whereever
-		document.documentElement.appendChild(node);
-	}
-}
+        GM_addStyle(css);
+    } else if (typeof PRO_addStyle != "undefined") {
+        PRO_addStyle(css);
+    } else if (typeof addStyle != "undefined") {
+        addStyle(css);
+    } else {
+        var node = document.createElement("style");
+        node.type = "text/css";
+        node.appendChild(document.createTextNode(css));
+        var heads = document.getElementsByTagName("head");
+        if (heads.length > 0) {
+            heads[0].appendChild(node);
+        } else {
+            // no head yet, stick it whereever
+            document.documentElement.appendChild(node);
+        }
+    }
+// ===== End WIDE =====
+
+// ===== Remove OVERLAY =====
+    const selectors = [
+        'div[data-type="portal"] div'
+    ];
+
+    function removeOverlay() {
+        for (const sel of selectors) {
+            document.querySelectorAll(sel).forEach(el => el.remove());
+        }
+        //document.body.style.overflow = 'auto';
+        //document.documentElement.style.overflow = 'auto';
+    }
+
+    removeOverlay();
+
+    const observer = new MutationObserver(() => {
+        removeOverlay();
+    });
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+// ===== End OVERLAY =====
+
 })();
