ee70d1e179a393cc4bd9ebfcb0f291333834bd30
zacisco <zacisco@users.noreply.github.com>
2026-08-14 16:44:38 +0300

    Update and rename WebPerplexityAI.js to WebPerplexityAI.user.js

--- user-scripts/WebPerplexityAI.user.js
+++ user-scripts/WebPerplexityAI.user.js
@@ -5,8 +5,8 @@
 // @author        Z@C
 // @icon          https://www.perplexity.ai/favicon.svg
 // @match         https://www.perplexity.ai/*
-// @updateURL     https://raw.githubusercontent.com/zacisco/notes/master/user-scripts/WebPerplexity.js
-// @downloadURL   https://raw.githubusercontent.com/zacisco/notes/master/user-scripts/WebPerplexity.js
+// @updateURL     https://raw.githubusercontent.com/zacisco/notes/master/user-scripts/WebPerplexity.user.js
+// @downloadURL   https://raw.githubusercontent.com/zacisco/notes/master/user-scripts/WebPerplexity.user.js
 // @run-at        document-end
 // ==/UserScript==
 
